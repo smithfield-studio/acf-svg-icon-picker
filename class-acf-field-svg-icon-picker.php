175f4b77bee8af6223e88dcb42c12e2049b21904
Erik van der Bas <erik@basedonline.nl>
2024-11-05 20:03:36 +0100

    wip: break up the constructor function in smaller pieces

--- class-acf-field-svg-icon-picker.php
+++ class-acf-field-svg-icon-picker.php
@@ -2,13 +2,11 @@
 
 /**
  * Field class for the SVG Icon Picker field.
- *
- * @package Advanced Custom Fields: SVG Icon Picker
  */
 
 namespace SmithfieldStudio\AcfSvgIconPicker;
 
-if (! defined('ABSPATH')) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
@@ -16,8 +14,8 @@ if (! defined('ABSPATH')) {
  * Field class for the SVG Icon Picker field.
  */
 class ACF_Field_Svg_Icon_Picker extends \acf_field
-{
 
+{
 	/**
 	 * Controls field type visibility in REST requests.
 	 *
@@ -26,33 +24,24 @@ class ACF_Field_Svg_Icon_Picker extends \acf_field
 	public $show_in_rest = true;
 
 	/**
-	 * Stores the path suffix to the icons
-	 *
-	 * @var string
+	 * Stores the path suffix to the icons.
 	 */
 	private string $path_suffix;
 
 	/**
-	 * Stores the path to the icons
-	 *
-	 * @var string
+	 * Stores the path to the icons.
 	 */
 	private string $path;
 
 	/**
-	 * Stores the url to the icons
-	 *
-	 * @var string
+	 * Stores the url to the icons.
 	 */
 	private string $url;
 
 	/**
-	 * Stores the icons
-	 *
-	 * @var array
+	 * Stores the icons.
 	 */
-	private array $svgs = array();
-
+	private array $svgs = [];
 
 	/**
 	 * Constructor.
@@ -61,99 +50,86 @@ class ACF_Field_Svg_Icon_Picker extends \acf_field
 	 */
 	public function __construct()
 	{
-
 		$this->name        = 'svg_icon_picker';
 		$this->label       = __('SVG Icon Picker', 'acf-svg-icon-picker');
 		$this->category    = 'content';
-		$this->defaults    = array('initial_value' => '');
-		$this->l10n        = array('error' => __('Error!', 'acf-svg-icon-picker'));
-		$this->path_suffix = apply_filters('acf_icon_path_suffix', 'resources/icons/');
-
-		//$this->path        = apply_filters('acf_icon_path', ACF_SVG_ICON_PICKER_PATH) . $this->path_suffix;
+		$this->defaults    = ['initial_value' => ''];
+		$this->l10n        = ['error' => __('Error!', 'acf-svg-icon-picker')];
 		$this->url         = get_stylesheet_directory_uri();
+		$this->path_suffix = apply_filters('acf_svg_icon_picker_folder', 'resources/icons/');
+		$this->path_suffix = apply_filters_deprecated('acf_icon_path_suffix', [$this->path_suffix], '4.0.0', 'acf_svg_icon_picker_folder');
 
-		$priority_dir_settings = apply_filters('acf_svg_icon_picker_custom_location', false);
+		// deprecate these two filters
+		apply_filters('acf_icon_path', ACF_SVG_ICON_PICKER_PATH) . $this->path_suffix;
+		apply_filters('acf_icon_url', ACF_SVG_ICON_PICKER_URL) . $this->path_suffix;
 
-		if (is_array($priority_dir_settings)) {
-			// see if path and url are set, if not throw an error
-			if (! isset($priority_dir_settings['path']) || ! isset($priority_dir_settings['url'])) {
-				wp_die(__('The path and url for the custom icon location must be set in the acf_svg_icon_picker_custom_location filter.', 'acf-svg-icon-picker'));
-			}
-
-			$this->path = $priority_dir_settings['path'];
-			$this->url  = $priority_dir_settings['url'];
-
-			$this->svgs = $this->svg_collector($this->path);
-			return;
+		/**
+		 * Check if the custom icon location is set by filter and if not, check the theme directories for icons.
+		 */
+		if (false === $this->check_priority_dir()) {
+			$this->check_theme_dirs();
 		}
-
-		$parent_theme_path = get_template_directory() . '/' . $this->path_suffix;
-		$child_theme_path  = get_stylesheet_directory() . '/' . $this->path_suffix;
-
-		if ($parent_theme_path !== $child_theme_path) {
-			$parent_svgs = $this->svg_collector($parent_theme_path, true);
-			$child_svgs  = $this->svg_collector($child_theme_path);
-
-			$this->svgs = array_merge($parent_svgs, $child_svgs);
-		} else {
-			$this->svgs = $this->svg_collector($parent_theme_path);
-		}
-
 
 		parent::__construct();
 	}
 
 	/**
-	 * Collects the icons from the specified path.
-	 * @var string $path The path to the icons to scan for SVG files.
+	 * Method that checks if the custom icon location is set by filter.
+	 * If it is, it sets the path and url to the custom location and collects the icons in that specific location.
+	 *
+	 * @throws \Exception if the path or url for the custom icon location are not set
+	 * @return boolean
+	 */
+	private function check_priority_dir(): bool
+	{
+		$priority_dir_settings = apply_filters('acf_svg_icon_picker_custom_location', false);
+
+		if (!is_array($priority_dir_settings)) {
+			return false;
+		}
+
+		if (!isset($priority_dir_settings['path']) || !isset($priority_dir_settings['url'])) {
+			throw new \Exception(__('The path and url for the custom icon location must be set in the acf_svg_icon_picker_custom_location filter.', 'acf-svg-icon-picker'));
+			return false;
+		}
+
+		$this->path = $priority_dir_settings['path'];
+		$this->url  = $priority_dir_settings['url'];
+		$this->svgs = $this->svg_collector($this->path, $this->url);
+		return true;
+	}
+
+	/**
+	 * Method that checks the theme directories for icons.
+	 *
 	 * @return array
 	 */
-	private function svg_collector(string $path, mixed $uri_location = false): array
+	private function check_theme_dirs(): array
 	{
-		$svg_files = array();
-		if (! is_dir($path)) {
-			return array();
+		$parent_theme_path = get_template_directory() . '/' . $this->path_suffix;
+		$child_theme_path  = get_stylesheet_directory() . '/' . $this->path_suffix;
+
+		$parent_theme_url = get_template_directory_uri() . '/' . $this->path_suffix;
+		$child_theme_url  = get_stylesheet_directory_uri() . '/' . $this->path_suffix;
+
+		$this->svgs = $this->svg_collector($parent_theme_path, $parent_theme_url);
+
+		if ($parent_theme_path !== $child_theme_path) {
+			$child_svgs = $this->svg_collector($child_theme_path, $child_theme_url);
+			$this->svgs = array_merge($this->svgs, $child_svgs);
+			$this->svgs = array_unique($this->svgs, SORT_REGULAR);
 		}
 
-		if (true === $uri_location) {
-			$url = get_template_directory_uri() . '/' . $this->path_suffix;
-		} else {
-			$url = $this->url . '/' . $this->path_suffix;
-		}
-
-		$found_files = array_filter(scandir($path), function ($file) {
-			return pathinfo($file, PATHINFO_EXTENSION) === 'svg';
-		});
-
-		if (empty($found_files)) {
-			return array();
-		}
-
-		foreach ($found_files as $key => $file) {
-			$name     = explode('.', $file)[0];
-			$filename = pathinfo($file, PATHINFO_FILENAME);
-			$name = str_replace(['-', '_'], ' ', $name);
-
-			$svg_files[$name] = array(
-				'name'     => $name,
-				'filename' => $filename,
-				'icon'     => $file,
-				'url'      => $url . $file,
-			);
-		}
-
-		return $svg_files;
+		return $this->svgs;
 	}
 
 	/**
 	 * Method that renders the field in the admin.
 	 *
-	 * @param array $field The field array.
-	 * @return void
+	 * @param array $field the field array
 	 */
 	public function render_field($field)
 	{
-
 		$input_icon = '' !== $field['value'] ? $field['value'] : $field['initial_value'];
 		$svg        = locate_template($this->path_suffix . $input_icon . '.svg');
 		$svg_exists = file_exists($svg);
@@ -165,9 +141,11 @@ class ACF_Field_Svg_Icon_Picker extends \acf_field
 				<div class="acf-svg-icon-picker__icon">
 					<?php echo $svg_exists ? '<img src="' . esc_url($svg_url) . '" alt=""/>' : '<span>&plus;</span>'; ?>
 				</div>
-				<input type="hidden" readonly name="<?php echo esc_attr($field['name']); ?>" value="<?php echo esc_attr($input_icon); ?>" />
+				<input type="hidden" readonly
+					name="<?php echo esc_attr($field['name']); ?>"
+					value="<?php echo esc_attr($input_icon); ?>" />
 			</div>
-			<?php if (! $field['required']) { ?>
+			<?php if (!$field['required']) { ?>
 				<button class="acf-svg-icon-picker__remove">
 					<?php esc_html_e('Remove', 'acf-svg-icon-picker'); ?>
 				</button>
@@ -178,33 +156,67 @@ class ACF_Field_Svg_Icon_Picker extends \acf_field
 
 	/**
 	 * Enqueue assets for the field.
-	 *
-	 * @return void
 	 */
 	public function input_admin_enqueue_scripts()
 	{
 		$url = ACF_SVG_ICON_PICKER_URL;
-		wp_register_script('acf-input-svg-icon-picker', "{$url}assets/js/input.js", array('acf-input'), ACF_SVG_ICON_PICKER_VERSION, true);
+		wp_register_script('acf-input-svg-icon-picker', "{$url}assets/js/input.js", ['acf-input'], ACF_SVG_ICON_PICKER_VERSION, true);
 		wp_enqueue_script('acf-input-svg-icon-picker');
 
 		wp_localize_script(
 			'acf-input-svg-icon-picker',
 			'acfSvgIconPicker',
-			array(
-				'path'      => $this->url,
-				'svgs'      => $this->svgs,
-				'columns'   => 4,
-				'msgs'      => array(
-					'title'     => esc_html__('Select an icon', 'acf-svg-icon-picker'),
-					'close'     => esc_html__('close', 'acf-svg-icon-picker'),
-					'filter'     => esc_html__('Start typing to filter icons', 'acf-svg-icon-picker'),
-					/* translators: %s: path_suffix */
-					'no_icons'  => sprintf(esc_html__('To add icons, add your svg files in the /%s folder in your theme.', 'acf-svg-icon-picker'), $this->path_suffix),
-				),
-			)
+			[
+				'svgs'    => $this->svgs,
+				'columns' => 4,
+				'msgs'    => [
+					'title'    => esc_html__('Select an icon', 'acf-svg-icon-picker'),
+					'close'    => esc_html__('close', 'acf-svg-icon-picker'),
+					'filter'   => esc_html__('Start typing to filter icons', 'acf-svg-icon-picker'),
+					// translators: %s: path_suffix
+					'no_icons' => sprintf(esc_html__('To add icons, add your svg files in the /%s folder in your theme.', 'acf-svg-icon-picker'), $this->path_suffix),
+				],
+			]
 		);
 
-		wp_register_style('acf-input-svg-icon-picker', "{$url}assets/css/input.css", array('acf-input'), ACF_SVG_ICON_PICKER_VERSION);
+		wp_register_style('acf-input-svg-icon-picker', "{$url}assets/css/input.css", ['acf-input'], ACF_SVG_ICON_PICKER_VERSION);
 		wp_enqueue_style('acf-input-svg-icon-picker');
 	}
+
+	/**
+	 * Collects the icons from the specified path.
+	 *
+	 * @var string the path to the icons to scan for SVG files
+	 */
+	private function svg_collector(string $path, string $url): array
+	{
+		$svg_files = [];
+		if (!is_dir($path)) {
+			return [];
+		}
+
+		$found_files = array_filter(scandir($path), function ($file) {
+			return 'svg' === pathinfo($file, PATHINFO_EXTENSION);
+		});
+
+		if (empty($found_files)) {
+			return [];
+		}
+
+		foreach ($found_files as $key => $file) {
+			$name     = explode('.', $file)[0];
+			$filename = pathinfo($file, PATHINFO_FILENAME);
+			$name     = str_replace(['-', '_'], ' ', $name);
+
+			$svg_files[$name] = [
+				'name'     => $name,
+				'filename' => $filename,
+				'icon'     => $file,
+				'url'      => $url . $file,
+			];
+		}
+
+		return $svg_files;
+	}
 }
+?>
