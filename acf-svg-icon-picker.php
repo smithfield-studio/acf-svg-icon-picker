2f92577492ac713968c5ae8546977f5769af79b7
Erik van der Bas <erik@basedonline.nl>
2026-04-28 14:21:38 +0200

    chore: Update version number to 4.3.1 and improve code formatting

--- acf-svg-icon-picker.php
+++ acf-svg-icon-picker.php
@@ -4,7 +4,7 @@
  * Plugin Name: Advanced Custom Fields: SVG Icon Picker
  * Plugin URI: https://github.com/smithfield-studio/acf-svg-icon-picker
  * Description: Allows you to pick an icon from a predefined list
- * Version: 4.3.0
+ * Version: 4.3.1
  * Author: Smithfield & Studio Lemon
  * Author URI: https://github.com/smithfield-studio/acf-svg-icon-picker/
  * Text Domain: acf-svg-icon-picker
@@ -19,31 +19,30 @@
 
 namespace SmithfieldStudio\AcfSvgIconPicker;
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Change this version number and the version in the
  * docblock above when releasing a new version of this plugin.
  */
-define('ACF_SVG_ICON_PICKER_VERSION', '4.3.0');
+define( 'ACF_SVG_ICON_PICKER_VERSION', '4.3.1' );
 
-define('ACF_SVG_ICON_PICKER_URL', plugin_dir_url(__FILE__));
-define('ACF_SVG_ICON_PICKER_PATH', plugin_dir_path(__FILE__));
+define( 'ACF_SVG_ICON_PICKER_URL', plugin_dir_url( __FILE__ ) );
+define( 'ACF_SVG_ICON_PICKER_PATH', plugin_dir_path( __FILE__ ) );
 
 /**
  * Include SVG Icon Picker field type.
  */
-function include_field_types(): void
-{
-	if (! function_exists('acf_register_field_type')) {
+function include_field_types(): void {
+	if ( ! function_exists( 'acf_register_field_type' ) ) {
 		return;
 	}
 
 	require_once __DIR__ . '/class-acf-field-svg-icon-picker.php';
-	acf_register_field_type('SmithfieldStudio\AcfSvgIconPicker\ACF_Field_Svg_Icon_Picker');
+	acf_register_field_type( 'SmithfieldStudio\AcfSvgIconPicker\ACF_Field_Svg_Icon_Picker' );
 }
 
-add_action('acf/include_field_types', __NAMESPACE__ . '\\include_field_types');
+add_action( 'acf/include_field_types', __NAMESPACE__ . '\\include_field_types' );
 
 
 /**
@@ -54,15 +53,14 @@ add_action('acf/include_field_types', __NAMESPACE__ . '\\include_field_types');
  * @param string $icon_name The name of the icon we want to get the URI for.
  * @return string The URI of the icon, empty string if the icon does not exist.
  */
-function get_svg_icon_uri(string $icon_name): string
-{
-	if (false == get_svg_icon_path($icon_name)) {
+function get_svg_icon_uri( string $icon_name ): string {
+	if ( false == get_svg_icon_path( $icon_name ) ) {
 		return '';
 	}
 
-	$location = apply_filters('acf_svg_icon_picker_folder', 'icons/');
+	$location = apply_filters( 'acf_svg_icon_picker_folder', 'icons/' );
 
-	return get_theme_file_uri("{$location}{$icon_name}.svg");
+	return get_theme_file_uri( "{$location}{$icon_name}.svg" );
 }
 
 /**
@@ -72,19 +70,18 @@ function get_svg_icon_uri(string $icon_name): string
  * @param string $icon_name The name of the icon we want to get the path for.
  * @return string The path of the icon, empty string if the icon does not exist.
  */
-function get_svg_icon_path(string $icon_name): string
-{
-	$priority_dir_settings = apply_filters('acf_svg_icon_picker_custom_location', false);
+function get_svg_icon_path( string $icon_name ): string {
+	$priority_dir_settings = apply_filters( 'acf_svg_icon_picker_custom_location', false );
 
-	if ($priority_dir_settings) {
+	if ( $priority_dir_settings ) {
 		$file_path = $priority_dir_settings['path'] ?? '';
-		$file_path = trailingslashit($file_path) . $icon_name . '.svg';
+		$file_path = trailingslashit( $file_path ) . $icon_name . '.svg';
 	} else {
-		$path      = apply_filters('acf_svg_icon_picker_folder', 'icons/');
-		$file_path = get_theme_file_path("{$path}{$icon_name}.svg");
+		$path      = apply_filters( 'acf_svg_icon_picker_folder', 'icons/' );
+		$file_path = get_theme_file_path( "{$path}{$icon_name}.svg" );
 	}
 
-	if (! file_exists($file_path)) {
+	if ( ! file_exists( $file_path ) ) {
 		return '';
 	}
 
@@ -98,13 +95,12 @@ function get_svg_icon_path(string $icon_name): string
  * @param string $icon_name The name of the icon we want to get.
  * @return string The SVG icon file, empty string if the icon does not exist.
  */
-function get_svg_icon(string $icon_name): string
-{
-	$path = get_svg_icon_path($icon_name);
+function get_svg_icon( string $icon_name ): string {
+	$path = get_svg_icon_path( $icon_name );
 
-	if (! $path) {
+	if ( ! $path ) {
 		return '';
 	}
 
-	return file_get_contents($path);
+	return file_get_contents( $path );
 }
